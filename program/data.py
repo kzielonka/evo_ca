24cad82fe65f5c9aa699409e4eb8f7ac69c42c01
Piotr Rzepecki <me@vanzi.info>
2013-01-23 11:42:02 +0100

    Fixed data input, .gitignore

--- program/data.py
+++ program/data.py
@@ -7,13 +7,13 @@ class Bid:
         self.__goods_list = goods_list
 
     @classmethod
-    def parse_line(cls, line):
+    def parse_line(cls, line, max):
         splited_line = re.split('\s+', line)
         identifier = int(splited_line[0])
         price = float(splited_line[1])
-        goods_list = map(lambda x: int(x), splited_line[2:-2])
+        goods_list = [i for i in map(lambda x: int(x), splited_line[2:-2]) if i < max]
         return cls(identifier, price, goods_list)
-        
+
     def identifier(self):
         return self.__identifier
 
@@ -31,9 +31,10 @@ class Bid:
 
 
 class CAInstance:
-    def __init__(self, goods_count, dummies_count, bids_list):
+    def __init__(self, goods_count, dummies_count, bids_count, bids_list):
         self.goods_count = goods_count
         self.dummies_count = dummies_count
+        self.bids_count = bids_count
         self.bids_list = bids_list
 
     @classmethod
@@ -49,8 +50,8 @@ class CAInstance:
             elif line[0] == 'd':
                 dummies = int(line.split(' ')[1])
             elif line[0] <= '9' and line[0] >= '0':
-                bids_list.append(Bid.parse_line(line))
-        return CAInstance(goods, dummies, bids_list)
+                bids_list.append(Bid.parse_line(line, goods))
+        return CAInstance(goods, dummies, bids, bids_list)
 
     @classmethod
     def __read_lines(cls, file_name):
@@ -67,7 +68,7 @@ class CAInstance:
 
     def solution_size(self):
         return len(self.bids_list)
-   
+
     def __str__(self):
         return "%s\n%s\n%s" % (self.goods_count, self.dummies_count, [str(x) for x in self.bids_list])
 
@@ -83,9 +84,22 @@ class CAInstance:
                     used_goods[i] = True
         return total_price
 
+    def evaluate_bin(self, binary_vector):
+        used_goods = [False] * self.all_goods_count()
+        total_price = 0.0
+        for i, b in enumerate(binary_vector):
+            if b:
+                bid = self.bids_list[i]
+                if bid.is_avalible(used_goods):
+                    total_price += bid.price()
+                    # updates used goods
+                    for i in bid.goods_identifiers():
+                        used_goods[i] = True
+        return total_price
 
-instance = CAInstance.from_file("0000.txt")
-print instance.evaluate([12, 11, 2,1,3,4,5,6,7,8,9,10, 0])
+if __name__ == '__main__':
+    instance = CAInstance.from_file("0000.txt")
+    print instance.evaluate([12, 11, 2,1,3,4,5,6,7,8,9,10, 0])
 
 # should be True
 #bid = Bid(1, 20, [0, 1])
